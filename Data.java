f02167c479e30fd3d7259369560f420b01bf790a
Mediha <mediha.munim@gmail.com>
2017-03-20 23:24:58 -0400

    Create Data.java
    This is a WIP

---
+++ Data.java
@@ -0,0 +1,133 @@
+package cas2xb3.greenlight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Data {
+
+	static private int size = getCollisionCount();
+	static private int[][] collisionArray = new int[size][6];
+	
+	public Data() {
+	}
+
+	/**
+	 * 
+	 * @return an array with only the important information. Also gives each row
+	 *         an index to be referenced by
+	 */
+	public static int[][] getArray() {
+
+		// If the array has already been filled:
+
+		if (collisionArray[0][0] != 0) {
+			return collisionArray;
+		}
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("Crash_Data.csv"));
+			String line;
+
+			// Put all the lines in the array (important information) with the
+			// crash index:
+			for (int i = 0; i < getCollisionCount(); i++) {
+				line = br.readLine();
+				if (line.charAt(0) == '2') { // Only consider the rows with
+												// crash information
+					String splitline[] = line.split(",");
+
+					collisionArray[i][0] = i;
+					collisionArray[i][1] = Integer.valueOf(splitline[36]); // XCOORD
+					collisionArray[i][2] = Integer.valueOf(splitline[37]); // YCOORD
+					if (!splitline[25].equals("")) {
+						collisionArray[i][3] = Integer.valueOf(splitline[25]); // CSEV
+					} else {
+						collisionArray[i][3] = 5; // assume non-severe collision
+					}
+					if (Integer.valueOf(splitline[20]) <= 10) {
+						collisionArray[i][4] = Integer.valueOf(splitline[20]); // WEATHER
+					} else {
+						collisionArray[i][4] = 5; // average weather
+					}
+					if (Integer.valueOf(splitline[23]) <= 2) {
+						collisionArray[i][5] = Integer.valueOf(splitline[23]); // PAVED
+					} else {
+						collisionArray[i][5] = 1; // assume paved
+					}
+				}
+			}
+			br.close();
+		} catch (IOException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		return collisionArray;
+	}
+
+	/**
+	 * @return the total number of collisions
+	 */
+	public static int getCollisionCount() {
+
+		// If the size has already been calculated:
+		if (size != 0) {
+			return size;
+		}
+
+		// The first time it's calculated:
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("Crash_Data.csv"));
+
+			br.readLine(); // skip the first line in the dataset
+			// Iterate through file line by line:
+			while (br.readLine().charAt(0) == '2') {
+				size++;
+			}
+
+			br.close();
+		} catch (IOException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		return size;
+	}
+	
+	public static int getXcrd(int crashNumb) {
+		return collisionArray[crashNumb][1];
+	}
+
+	public static int getYcrd(int crashNumb) {
+		return collisionArray[crashNumb][2];
+	}
+
+	public static int getCSEV(int crashNumb) {
+		return collisionArray[crashNumb][3];
+	}
+
+	public static int getWeather(int crashNumb) {
+		return collisionArray[crashNumb][4];
+	}
+
+	public static int getPaved(int crashNumb) {
+		return collisionArray[crashNumb][5];
+	}
+
+	public static void printArray() {
+		for (int i = 0; i < collisionArray.length; i++) {
+			for (int j = 0; j < 6; j++) {
+				System.out.print(collisionArray[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	/**
+	 * crashNumb is the index of the crash in collisionArray
+	 * @return 
+	 */
+	public static double equation(int crashNumb) {
+		return (1.0/(2*getCSEV(crashNumb))+ 1/(5*getWeather(crashNumb)))*(0.9+(getPaved(crashNumb)/10));
+	}
+
+}
