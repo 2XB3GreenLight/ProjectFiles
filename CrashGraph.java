a54b5c1539f7946d4d14a603d7fe1026c056ca81
AbrarAttia <abrarattia@gmail.com>
2017-04-09 12:59:11 -0700

    Add files via upload

--- CrashGraph.java
+++ CrashGraph.java
@@ -1,51 +1,62 @@
 package cas2xb3.greenlight;
 
 import java.io.IOException;
+import edu.princeton.cs.algs4.*;
 
+/**
+ * A class which represents a graph connecting all of the collisions to
+ * replicate the roads of Iowa.
+ * 
+ * @author Abrar Attia
+ * @author Mediha Munim
+ */
 public class CrashGraph {
 
 	private static Collision[] crashArr;
 
-	// Calculates the severity of the inputed crash point
-	public static double crashSeverity(double[] crashPoint) {
-
-		double indSeverity = 1 / (2.0 * crashPoint[3]);
-		double weather = 1 / (5.0 * crashPoint[4]);
-		double paved = 0.9 + (crashPoint[5] / 10.0);
-
-		double totalSeverity = indSeverity + weather + paved;
-		return totalSeverity;
-	}
-
-	// The 4 crashes adjacent to the inputed crash
-	public static int[] NSWECrashes(int crashIndex) {
+	/**
+	 * Calculates the indices of the four collisions that are adjacent to the
+	 * given collision (to the North, South, East and West).
+	 * 
+	 * @param crashIndex
+	 *            The particular collision index
+	 * @return
+	 */
+	private static int[] NSWECrashes(int crashIndex) {
 		crashArr = Data.getArray();
 		int[] NSWEcrash = new int[4];
+		// Data point (90000) outside of reach used as null point
 		NSWEcrash[0] = 90000;
 		NSWEcrash[1] = 90000;
 		NSWEcrash[2] = 90000;
 		NSWEcrash[3] = 90000;
+		// Maximum distance between two adjacent nodes
 		double distN = 100.0;
 		double distS = 100.0;
 		double distW = 100.0;
 		double distE = 100.0;
 
-		int low = crashIndex - 10000;
-		int hi = crashIndex + 10000;
-		if (low < 0) low = 0;
-		if (hi > Data.getCollisionCount()) hi = Data.getCollisionCount();
-		
+		// Reduces the size of the crash data
+		int low = crashIndex - 5000;
+		int hi = crashIndex + 5000;
+		if (low < 0)
+			low = 0;
+		if (hi > Data.getCollisionCount())
+			hi = Data.getCollisionCount();
+
+		// Compares all the crash nodes within the low and high range
 		for (int i = low; i < hi; i++) {
+			// Stores the latitude and longitude of crash in reference and one being compared
 			double otherLat = crashArr[i].getLat();
 			double thisLat = crashArr[crashIndex].getLat();
 			double otherLong = crashArr[i].getLng();
 			double thisLong = crashArr[crashIndex].getLng();
+			// Stores the spotlight range to find crashes 
 			double posEdge = otherLong + (thisLat - thisLong);
 			double negEdge = -otherLong + (thisLat + thisLong);
-			
+
 			// NORTHERN REGION
 			if ((otherLat - thisLat) <= distN && otherLat > posEdge && otherLat > negEdge) {
-				// if (otherLat > thisLat && (otherLat - thisLat) <= distN) {
 				distN = otherLat - thisLat;
 				NSWEcrash[0] = i;
 			}
@@ -67,36 +78,53 @@ public class CrashGraph {
 				NSWEcrash[3] = i;
 			}
 		}
-
+		//Returns the closest northern, southern, western, and eastern crashes to the crash in reference
 		return NSWEcrash;
 	}
 
+	/**
+	 * Creates a graph from all of the collisions by connecting each collision
+	 * to the points North, South, East and West of it.
+	 * 
+	 * @return The graph of the collisions.
+	 * @throws IOException
+	 */
 	public static EdgeWeightedDigraph FinalGraph() throws IOException {
 		EdgeWeightedDigraph G = new EdgeWeightedDigraph(Data.getCollisionCount());
 		crashArr = Data.getArray();
 		for (int i = 0; i < G.V(); i++) {
+			// Determines the 4 crashes each crash will be connected to
 			int[] NSWEcrash = NSWECrashes(i);
 			int n = NSWEcrash[0];
 			int s = NSWEcrash[1];
 			int w = NSWEcrash[2];
 			int e = NSWEcrash[3];
+			// Confirms that the crash is not connected to a null point
 			if (n != 90000) {
+				// Creates edge from the crash in reference to the closest northern crash
 				DirectedEdge northC = new DirectedEdge(i, n, (crashArr[i]).getEquation() + (crashArr[n]).getEquation());
 				G.addEdge(northC);
 			}
+			// Confirms that the crash is not connected to a null point
 			if (s != 90000) {
+				// Creates edge from the crash in reference to the closest southern crash
 				DirectedEdge southC = new DirectedEdge(i, s, (crashArr[i]).getEquation() + (crashArr[s]).getEquation());
 				G.addEdge(southC);
 			}
+			// Confirms that the crash is not connected to a null point
 			if (w != 90000) {
+				// Creates edge from the crash in reference to the closest western crash
 				DirectedEdge westC = new DirectedEdge(i, w, (crashArr[i]).getEquation() + (crashArr[w]).getEquation());
 				G.addEdge(westC);
 			}
+			// Confirms that the crash is not connected to a null point
 			if (e != 90000) {
+				// Creates edge from the crash in reference to the closest eastern crash
 				DirectedEdge eastC = new DirectedEdge(i, e, (crashArr[i]).getEquation() + (crashArr[e]).getEquation());
 				G.addEdge(eastC);
 			}
 		}
+		// Returns the complete graph
 		return G;
 	}
 }
